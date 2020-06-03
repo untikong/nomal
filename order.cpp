5b176d9dc47bd570aaa1eb8ac679ca544ac7691d
untikong <63002739+untikong@users.noreply.github.com>
2020-06-03 18:40:48 +0900

    Create order.cpp

---
+++ order.cpp
@@ -0,0 +1,11 @@
+ map<ll,int> ord;  //素因数とオーダーをマップで管理
+    for(ll i=2;i*i<n;i++){
+        if(n%i==0){
+            int res=0;
+            while(n%i==0){
+                n/=i;
+                res++;
+            }
+            ord[i]=res;
+        }
+    }
