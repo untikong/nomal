b5a102f8b2eeab0ae5e18670225ff88c4a9125ec
untikong <63002739+untikong@users.noreply.github.com>
2020-06-05 01:10:13 +0900

    Update lds.cpp

--- lds.cpp
+++ lds.cpp
@@ -1,8 +1,8 @@
 ll lds(vector<ll> a){
  ll n=a.size() ;
  deque<ll> d;
- rep(i,n){
-    ll p=lower_bound(d.begin(),d.end(),a[i])-d.begin();
+ rep(i,n){  //広義はlower 狭義はupper
+    ll p=lower_bound(d.begin(),d.end(),a[i])-d.begin(); 
     if(p==0) d.push_front(a[i]) ;
     else d[p-1]=a[i] ;
  }
