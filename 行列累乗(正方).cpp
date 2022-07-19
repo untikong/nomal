e54d6e5a9013b1d8481432c821ce2c90041a97c4
untikong <63002739+untikong@users.noreply.github.com>
2022-07-19 18:49:41 +0900

    Update 行列累乗(正方).cpp

--- 行列累乗(正方).cpp
+++ 行列累乗(正方).cpp
@@ -1,24 +1,24 @@
 //縦横nの正方行列Aのx乗
+
 ll n ;
 vector<vector<mint>> mat(vector<vector<mint>>&A,ll x){
+  vector<vector<mint>> zero(n,vector<mint> (n,0));
+  rep(i,n) zero[i][i]=1;
+  if(x==0) return zero;
   if(x==1) return  A ;
   vector<vector<mint>> B(n,vector<mint>(n,0)) ;
   vector<vector<mint>> C(n,vector<mint>(n,0)) ;
   vector<vector<mint>> V=mat(A,x/2) ;
-   rep(i,n)rep(j,n){
-     mint now=0 ;
-     rep(k,n) now+=V[i][k]*V[k][j] ;
-     B[i][j]=now ;
-   }
-   if(x%2==0){
-     return B ;
-   }
-    else{
+  rep(i,n)rep(j,n){
+    rep(k,n) B[i][j]+=V[i][k]*V[k][j] ;
+  }
+  if(x%2==0){
+    return B ;
+  }
+  else{
       rep(i,n)rep(j,n){
-      mint now=0ll ;
-      rep(k,n) now+=B[i][k]*A[k][j] ;
-      C[i][j]=now ;
-    }
-    return C ;
-   }
+        rep(k,n) C[i][j]+=B[i][k]*A[k][j] ;
+      }
+      return C ;
+  }
 }
